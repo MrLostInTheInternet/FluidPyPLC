7649f83148a9844d8d41de53a9a980f05a68cf3b
MrLostInTheInternet <ubuntutest10@gmail.com>
2023-06-01 23:01:44 +0200

    Up to date
    Cleaned the fluid.py code. Initialized the layout of GUI

--- GUI.py
+++ GUI.py
@@ -1,1 +1,41 @@
+from get_sequence import *
+
 import PySimpleGUI as sg
+
+def collapse(layout, key, visible):
+    return sg.pin(sg.Column(layout, key=key, visible=visible))
+
+class Gui():
+    def __init__(self):
+        self.s = []
+    def run(self):
+        section = [[sg.Multiline(expand_x = True, horizontal_scroll=True, size = (50, 19), key = 'plc_code')]]
+
+        headings = ('Block S.','Groups', 'N. Memories','Relay Memories', 'L. S. Enabled', 'L. S. Disabled')
+
+        image_viewer_column = [
+            [sg.Table(values = data , headings = headings, size = (10, 6), auto_size_columns=True, expand_x = True, visible = False, key = 'table')],
+            [sg.Image(key="-IMAGE-", visible = False, expand_x = True)]
+        ]
+
+        layout_data = [[sg.Text('Sequence:', size = (11, 2)), sg.Text(key = 'text', expand_x = True, size = (35, 2), text_color = 'Black')],
+                    [sg.Text('Insert stroke: ', size = (11, 2)), sg.Input(key = 'input', size = (6, 2), text_color='Black')],
+                    [sg.Button('Finish', size =(10, 1)), sg.Button('Clear', size = (10, 1)), sg.Button("Display Diagram's fases", expand_x = True, expand_y = True)],
+                    [sg.Checkbox('Show PLC ST code', enable_events=True, key = 'checkbox_key'), sg.Checkbox('Show Data', enable_events=True, key = 'data')],
+                    [collapse(section, 'plc', False)],
+                ]
+
+        layout = [
+            [
+                sg.Column(layout_data, vertical_alignment= 'top'),
+                sg.VSeperator(),
+                sg.Column(image_viewer_column, visible = False, key = 'image_column'),
+            ]
+        ]
+
+        sequence = []
+
+        window = sg.Window('GUI', layout, finalize = True)
+        window['input'].bind("<Return>", "_Enter")
+
+        # while True
--- fluid.py
+++ fluid.py
@@ -9,6 +9,7 @@ import argparse
 import subprocess
 import textwrap
 
+# class to define the plot rendering and opening window
 class Window(Frame):
     def __init__(self,  master=None):
         Frame.__init__(self, master)
@@ -21,7 +22,22 @@ class Window(Frame):
         img.image = render
         img.place(x=0, y=0)
 
+# function to start the terminal version
+def terminal():
+    sequence = Sequence()
+    s = sequence.s
+    diagrams(s)
+    Plc(s)
 
+# show Diagram's fases
+def show_plot():
+    root = Tk()
+    app = Window(root)
+    root.wm_title("Diagram's fases")
+    root.geometry("600x500")
+    root.mainloop()
+
+# args management
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='FluidPy 2.0',
@@ -30,30 +46,31 @@ if __name__ == '__main__':
         fluid.py --gui # to use the user interface mode
         fluid.py -t # to use the terminal version
         fluid.py --plot # to display the Diagrams's fases
-        fluid.py --plc # to display the plc ST code''')
+        fluid.py --plc # to display the plc ST code
+        ''')
     )
-    parser.add_argument('-g', '--gui', action='store_true', help='gui')
-    parser.add_argument('-t', '--terminal', action='store_true', help='command shell')
+    parser.add_argument('-g', '--gui', action='store_true', help='gui mode')
+    parser.add_argument('-t', '--terminal', action='store_true', help='terminal mode')
     parser.add_argument('--plot', action='store_true', help='show plot')
     parser.add_argument('--plc', action='store_true', help='show plc code')
     args = parser.parse_args()
     if args.gui:
+        # gui mode
         '''Gui = GUI()
         Gui.run()'''
         print('gui')
     elif args.terminal:
-        sequence = Sequence()
-        s = sequence.s
-        diagrams(s)
-        Plc(s)
+        # terminal mode
+        terminal()
     elif args.plot:
-        root = Tk()
-        app = Window(root)
-        root.wm_title("Diagram's fases")
-        root.geometry("600x500")
-        root.mainloop()
+        # show plot
+        show_plot()
     elif args.plc:
         try:
+            # open plc ST code with notepad
             subprocess.call(['notepad.exe', './plc/plc.txt'])
         except:
-            print("The file doesn't exist")
+            print("There is a problem opening the file.")
+    else:
+        # default argument
+        terminal()
