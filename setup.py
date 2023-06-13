c155fef90b38313541497a8aaf79f7a00512bbb1
MrLostInTheInternet <ubuntutest10@gmail.com>
2023-06-13 19:09:42 +0200

    Setup file

--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=['PySimpleGUI', 'matplotlib', 'tk', 'pillow'],
+    entry_points={
+        'console_scripts': [
+            'FluidPyPLC = FluidPyPLC.main:f',
+        ]},
     keywords=['python', 'plc', 'fluidsim', 'structured text', 'plc python', 'codesys', 'circuits', 'pneumatics', 'oleodynamics', 'plc programming'],
     classifiers=[
         "Development Status :: 1 - Planning",
