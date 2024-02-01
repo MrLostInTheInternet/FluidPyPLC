23d738ac7943e4e211b9e13542b29efc29cde001
Eugen Iofciu Vasile <74376532+MrLostInTheInternet@users.noreply.github.com>
2024-02-01 10:37:14 +0100

    (version) 0.1.11

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.109'
+VERSION = '0.1.11'
 DESCRIPTION = 'FluidSim Circuits Analyzer & PLC ST Code Generator'
 
 with open("FluidPyPLC/README.md", "r") as f:
