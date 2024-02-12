98c78e11b0d107c0c01bd529c891516ea5e3d9b8
Eugen Iofciu Vasile <74376532+MrLostInTheInternet@users.noreply.github.com>
2024-02-12 13:15:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = '0.3.500'
+VERSION = '0.3.501'
 DESCRIPTION = 'FluidSim Circuits Analyzer & PLC ST Code Generator'
 
 # Setting up
